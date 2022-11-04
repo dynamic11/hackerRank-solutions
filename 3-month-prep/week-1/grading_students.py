f6752e10bf6775440b1c5b1b7c1c5e82652ef9e5
Ali Nouri <dynamic11@gmail.com>
2022-11-04 14:38:44 -0400

    [Solved] Grading Students

---
+++ 3-month-prep/week-1/grading_students.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+def gradingStudents(grades):
+    for index, grade in enumerate(grades):
+        closest_multiple_of_five = math.ceil(grade/5)*5
+        if(closest_multiple_of_five-grade < 3 and grade >= 38):
+            grades[index]=closest_multiple_of_five
+
+    return grades
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
