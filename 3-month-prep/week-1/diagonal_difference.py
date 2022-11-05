6b544a7f7d9c71efe9188876e98f618a75cc2aa1
Ali Nouri <dynamic11@gmail.com>
2022-11-05 18:11:10 -0400

    [Solved] Diagonal Difference

---
+++ 3-month-prep/week-1/diagonal_difference.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    sum1 = sum2 = 0
+    array_length=len(arr[0])
+    
+    for i in range(array_length):
+        sum1 += arr[i][i]
+        sum2 += arr[i][array_length-i-1]
+        
+    return abs(sum1-sum2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
